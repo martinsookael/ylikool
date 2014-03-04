47f69f6ad60128fe1c8abd4252c715a72965f0c0
Martin Sookael <m@451.ee>
2014-03-04 10:49:16 +0200

    local changes

--- javascripts/script.js
+++ javascripts/script.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
             //var description = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce id tortor nisi. Aenean sodales diam ac lacus elementum scelerisque. Suspendisse a dui vitae lacus faucibus venenatis vel id nisl. Proin orci ante, ultricies nec interdum at, iaculis venenatis nulla. ';
 
             $('#player').ttwMusicPlayer(myPlaylist, {
-                autoplay: true, 
+                autoplay: false, 
                 //description:description,
                 jPlayer:{
                     swfPath:'../frameworks/music-player/plugin/jquery-jplayer' //You need to override the default swf path any time the directory structure changes
