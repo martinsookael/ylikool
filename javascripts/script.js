1dc3282beec30abf4a0bbaf349e0c4e28a07ddfe
Argo Roots <argo@roots.ee>
2014-05-18 22:48:26 +0300

    Complete rewrite; Take data from ylikool.entu.ee;

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,196 +1,123 @@
-// JavaScript Document
+var entuURL = 'https://ylikool.entu.ee/api/';
 
-// shortcut for console.log
 function cl(data) {
     console.log(data);
 }
 
-var scotchApp = angular.module('scotchApp', ['ngRoute', 'ui.unique']);
+angular.module('ylikoolApp', ['ngRoute'])
 
-//var scotchApp = angular.module('scotchApp', ['ui.unique']);
-
-    scotchApp.config(function($sceDelegateProvider) {
-        $sceDelegateProvider.resourceUrlWhitelist([
-        // Allow same origin resource loads.
-        'self',
-        // Allow loading from our assets domain.  Notice the difference between * and **.
-        'http://heli.er.ee/helid/oy/**']);
-
-        // The blacklist overrides the whitelist so the open redirect here is blocked.
-        $sceDelegateProvider.resourceUrlBlacklist([
-        'http://myapp.example.com/clickThru**']);
-    });
-
-
-	// configure our routes
-	scotchApp.config(function($routeProvider) {
-		$routeProvider
-
-			// route for the home page
-			.when('/', {
-				templateUrl : 'pages/kroonika.html',
-				controller  : 'otsiController'
-			})
-
-			.when('/otsi', {
-				templateUrl : 'pages/otsi.html',
-				controller  : 'otsiController'
-			})
-
-            .when('/esinejad', {
-				templateUrl : 'pages/esinejad.html',
-				controller  : 'authorController'
-			})
-
-            .when('/esineja/:esineja', {
-				templateUrl : 'pages/esineja.html',
-				controller  : 'esinejaController'
-			})
-
-            .when('/teemad', {
-				templateUrl : 'pages/teemad.html',
-				controller  : 'otsiController'
-			})
-
-            .when('/teema/:tag', {
-				templateUrl : 'pages/teema.html',
-				controller  : 'tagController'
-			})
-
-            .when('/kroonika', {
-				templateUrl : 'pages/kroonika.html',
-				controller  : 'otsiController'
-			})
-
-            .when('/loeng/:name', {
-                templateUrl: 'pages/loeng.html',
-                controller: 'CMSController'
+    .config(['$routeProvider', '$sceDelegateProvider', function($routeProvider, $sceDelegateProvider) {
+        $routeProvider
+            .when('/ajatelg', {
+                templateUrl : 'pages/history.html',
+                controller  : 'listCtrl'
             })
-    });
+            .when('/konelejad', {
+                templateUrl : 'pages/authors.html',
+                controller  : 'listCtrl'
+            })
+            .when('/:id', {
+                templateUrl : 'pages/lecture.html',
+                controller  : 'lectureCtrl'
+            })
+            .otherwise({ redirectTo: '/ajatelg' });
 
-    scotchApp.filter('searchFilter', function() {
-        return function(lectures, searchText) {
+        $sceDelegateProvider.resourceUrlWhitelist([
+            'self',
+            'http://heli.er.ee/**'
+        ]);
+    }])
 
-            var regexp = new RegExp(searchText, 'i');
-            return lectures.filter(function(lecture) {
-                var found = false;
-                //!TODO: Add whatever other fields need to be searched
-                if (lecture.title.search(regexp) > -1) {
-                    found = true;
-                }
-                if (lecture.tag.search(regexp) > -1) {
-                    found = true;
-                }
-                if (!found) {
-                    lecture.authors.some(function(author) {
-                        found = author.search(regexp) > -1;
-                        return found;
-                    });
-                }
-
-                return found;
-            });
-        };
-    });
-
-
-    scotchApp.filter('relatedFilter', function() {
-        return function(lectures, searchText) {
-
-            var regexp = new RegExp(searchText, 'i');
-            return lectures.filter(function(lecture) {
-                var found = false;
-                //!TODO: Add whatever other fields need to be searched
-                if (lecture.tag.search(regexp) > -1) {
-                    found = true;
-                }
-                if (!found) {
-                    lecture.authors.some(function(author) {
-                        found = author.search(regexp) > -1;
-                        return found;
-                    });
-                }
-
-                return found;
-            });
-        };
-    });
-
-    scotchApp.run(function($rootScope, $templateCache) {
-       $rootScope.$on('$viewContentLoaded', function() {
-          $templateCache.removeAll();
-       });
-    });
-
-
-	scotchApp.controller('mainController', function($scope) {
+    .controller('mainCtrl', ['$rootScope', '$scope', '$location', function($rootScope, $scope, $location) {
+        $rootScope.url = $location.path().replace('/', '');
         $scope.nightTipNr = Math.floor((Math.random()*3)+1);
-
+        $scope.pages = [
+            {url: 'ajatelg',   title: 'Ajatelg'},
+            {url: 'konelejad', title: 'Kõnelejad'},
+            // {url: 'teemad',    title: 'Teemad'},
+        ];
 
         $scope.nightToggle = function () {
             $scope.nightMode = !$scope.nightMode;
             $scope.hideNightTip = true;
         }
-    });
+    }])
 
-    scotchApp.controller('otsiController', function($scope) {
-        $scope.db = db;
-    });
+    .controller('listCtrl', ['$rootScope', '$scope', '$http', '$filter', '$location', function($rootScope, $scope, $http, $filter, $location) {
+        $rootScope.url = $location.path().replace('/', '');
 
-    scotchApp.controller('authorController', function($scope) {
+        if($rootScope.lectures || $rootScope.years || $rootScope.authors) {
+            $scope.lectures = $rootScope.lectures;
+            $scope.years = $rootScope.years;
+            $scope.authors = $rootScope.authors;
+        } else {
+            $http({method: 'GET', url: entuURL+'get_entity_list', params: {entity_definition_keyname: 'loeng', only_public: true, full_info: true}}).success(function(data) {
+                $rootScope.lectures = [];
+                $rootScope.years = [];
+                $rootScope.authors = [];
 
-        // get only single authors from db
-        var singleAuthors = [];
-        db.forEach(function(entry) {
-            var authors = entry.authors.toString().split(", ");
-            authors.forEach(function(author){
-                if(singleAuthors.indexOf(author)===-1) {
-                    singleAuthors.push(author);
-                }
+                for(i in data) {
+                    try        { var date = data[i].properties.kuupaev.values[0].value; }
+                    catch(err) { var date = ''; }
+
+                    try        { var year = parseInt($filter('date')(data[i].properties.kuupaev.values[0].value, 'yyyy')); }
+                    catch(err) { var year = ''; }
+
+                    try        { var title = data[i].properties.nimi.values[0].value; }
+                    catch(err) { var title = ''; }
+
+                    try        { var authors = data[i].properties.esitaja.values; }
+                    catch(err) { var authors = []; }
+
+                    try        { var link = data[i].properties.link.values[0].value; }
+                    catch(err) { var link = ''; }
+
+                    if($scope.years.indexOf(year) == -1) {
+                        $rootScope.years.push(year);
+                        $rootScope.years = $rootScope.years.sort();
+                        $rootScope.years = $rootScope.years.reverse();
+                    }
+
+                    var item_authors = [];
+                    for(a in authors) {
+                        var author = authors[a].value;
+                        item_authors.push(author);
+                        if($rootScope.authors.indexOf(author) == -1) {
+                            $rootScope.authors.push(author);
+                            $rootScope.authors = $rootScope.authors.sort();
+                        }
+                    }
+
+                    $rootScope.lectures.push({
+                        id      : data[i].id,
+                        date    : date,
+                        year    : year,
+                        title   : title,
+                        authors : item_authors,
+                        link    : link,
+                    });
+
+                };
+
+                $scope.lectures = $rootScope.lectures;
+                $scope.years = $rootScope.years;
+                $scope.authors = $rootScope.authors;
             });
-        });
-        $scope.singleAuthors = singleAuthors.sort();
-    });
+        }
 
-    scotchApp.controller('tagController', function($scope, $route, $routeParams) {
-        $scope.tag = $routeParams.tag;
-        $scope.searchText = $scope.tag;
-        $scope.db = db;
-    });
+     }])
 
-    scotchApp.controller('esinejaController', function($scope, $route, $routeParams) {
-        $scope.esineja = $routeParams.esineja;
-        $scope.searchText = $scope.esineja;
-        $scope.db = db;
-    });
+    .controller('lectureCtrl', ['$rootScope', '$scope', '$routeParams', function($rootScope, $scope, $routeParams) {
+        $rootScope.url = $routeParams.id;
 
-    scotchApp.controller('CMSController', function($scope, $route, $routeParams) {
-        var nid = $routeParams.name;
-        var data = db[nid];
-        $scope.title = data.title;
-        $scope.tag = data.tag;
-        $scope.file = data.file;
-        $scope.fileUrl = "http://heli.er.ee/helid/oy/"+data.file+".mp3";
-        $scope.text = data.text;
-        $scope.year = data.year;
-        $scope.authors = data.authors;
-        $scope.sound = data.sound;
-        $scope.editor = data.editor;
-        $scope.nid = nid;
-
-        $scope.db = db;
-
-    });
-
-    scotchApp.directive('socialPlugins', ['$timeout', function ($timeout) {
-        return {
-            restrict: 'A',
-            scope: false,
-            templateUrl: 'pages/social-plugins.html',
-            link: function(scope, element, attributes) {
-                $timeout(function () {
-                    FB.XFBML.parse(element.parent()[0])
-                });
+        for(i in $rootScope.lectures) {
+            if($rootScope.lectures[i].id == $routeParams.id) {
+                $scope.lecture = $rootScope.lectures[i];
+                break;
             }
+        }
+
+        $scope.palyAudio = function(lecture) {
+            $rootScope.current_lecture = lecture;
         }
     }]);
