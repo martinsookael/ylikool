e913935e79206559148e3819be72be3ea59cbfb7
Argo Roots <argo@roots.ee>
2014-05-19 14:14:14 +0300

    Load data from API2; Loading progressbar;

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,7 +1,36 @@
-var entuURL = 'https://ylikool.entu.ee/api/';
+var entuURL = 'https://ylikool.entu.ee/api2/';
 
 function cl(data) {
     console.log(data);
+}
+
+function toLecture(data) {
+    try        { var date = data.properties.kuupaev.values[0].value; }
+    catch(err) { var date = ''; }
+
+    try        { var title = data.properties.nimi.values[0].value; }
+    catch(err) { var title = ''; }
+
+    try        { var authors = data.properties.esitaja.values; }
+    catch(err) { var authors = []; }
+
+    try        { var link = data.properties.link.values[0].value; }
+    catch(err) { var link = ''; }
+
+    var item_authors = [];
+    for(a in authors) {
+        item_authors.push(authors[a].value);
+    }
+
+    return {
+        id      : data.id,
+        date    : date,
+        year    : date.substring(0, 4),
+        title   : title,
+        authors : item_authors,
+        link    : link,
+    };
+
 }
 
 angular.module('ylikoolApp', ['ngRoute'])
@@ -43,79 +72,57 @@ angular.module('ylikoolApp', ['ngRoute'])
         }
     }])
 
-    .controller('listCtrl', ['$rootScope', '$scope', '$http', '$filter', '$location', function($rootScope, $scope, $http, $filter, $location) {
+    .controller('listCtrl', ['$rootScope', '$scope', '$http', '$location', function($rootScope, $scope, $http, $location) {
         $rootScope.url = $location.path().replace('/', '');
 
         if($rootScope.lectures || $rootScope.years || $rootScope.authors) {
             $scope.lectures = $rootScope.lectures;
-            $scope.years = $rootScope.years;
-            $scope.authors = $rootScope.authors;
+            $scope.years    = $rootScope.years;
+            $scope.authors  = $rootScope.authors;
         } else {
-            $http({method: 'GET', url: entuURL+'get_entity_list', params: {entity_definition_keyname: 'loeng', only_public: true, full_info: true}}).success(function(data) {
-                $rootScope.lectures = [];
-                $rootScope.years = [];
-                $rootScope.authors = [];
+            $http({method: 'GET', url: entuURL+'entity', params: {definition: 'loeng'}}).success(function(data) {
+                $rootScope.lectures_count  = data.result.length;
+                $rootScope.lectures_loaded = 0;
+                $rootScope.lectures        = [];
+                $rootScope.years           = [];
+                $rootScope.authors         = [];
 
-                for(i in data) {
-                    try        { var date = data[i].properties.kuupaev.values[0].value; }
-                    catch(err) { var date = ''; }
+                for(i in data.result) {
+                    $http({method: 'GET', url: entuURL+'entity-'+data.result[i].id}).success(function(data) {
+                        var lecture = toLecture(data.result);
 
-                    try        { var year = parseInt($filter('date')(data[i].properties.kuupaev.values[0].value, 'yyyy')); }
-                    catch(err) { var year = ''; }
+                        $rootScope.lectures.push(lecture);
 
-                    try        { var title = data[i].properties.nimi.values[0].value; }
-                    catch(err) { var title = ''; }
-
-                    try        { var authors = data[i].properties.esitaja.values; }
-                    catch(err) { var authors = []; }
-
-                    try        { var link = data[i].properties.link.values[0].value; }
-                    catch(err) { var link = ''; }
-
-                    if($scope.years.indexOf(year) == -1) {
-                        $rootScope.years.push(year);
-                        $rootScope.years = $rootScope.years.sort();
-                        $rootScope.years = $rootScope.years.reverse();
-                    }
-
-                    var item_authors = [];
-                    for(a in authors) {
-                        var author = authors[a].value;
-                        item_authors.push(author);
-                        if($rootScope.authors.indexOf(author) == -1) {
-                            $rootScope.authors.push(author);
-                            $rootScope.authors = $rootScope.authors.sort();
+                        if($rootScope.years.indexOf(lecture.year) == -1) {
+                            $rootScope.years.push(lecture.year);
+                            $rootScope.years = $rootScope.years.sort();
+                            $rootScope.years = $rootScope.years.reverse();
                         }
-                    }
 
-                    $rootScope.lectures.push({
-                        id      : data[i].id,
-                        date    : date,
-                        year    : year,
-                        title   : title,
-                        authors : item_authors,
-                        link    : link,
+                        for(a in lecture.authors) {
+                            if($rootScope.authors.indexOf(lecture.authors[a]) == -1) {
+                                $rootScope.authors.push(lecture.authors[a]);
+                                $rootScope.authors = $rootScope.authors.sort();
+                            }
+                        }
+
+                        $scope.lectures = $rootScope.lectures;
+                        $scope.years    = $rootScope.years;
+                        $scope.authors  = $rootScope.authors;
+
+                        $rootScope.lectures_loaded += 1;
                     });
-
                 };
-
-                $scope.lectures = $rootScope.lectures;
-                $scope.years = $rootScope.years;
-                $scope.authors = $rootScope.authors;
             });
         }
-
      }])
 
-    .controller('lectureCtrl', ['$rootScope', '$scope', '$routeParams', function($rootScope, $scope, $routeParams) {
+    .controller('lectureCtrl', ['$rootScope', '$scope', '$http', '$routeParams', function($rootScope, $scope, $http, $routeParams) {
         $rootScope.url = $routeParams.id;
 
-        for(i in $rootScope.lectures) {
-            if($rootScope.lectures[i].id == $routeParams.id) {
-                $scope.lecture = $rootScope.lectures[i];
-                break;
-            }
-        }
+        $http({method: 'GET', url: entuURL+'entity-'+$routeParams.id}).success(function(data) {
+            $scope.lecture = toLecture(data.result);
+        });
 
         $scope.palyAudio = function(lecture) {
             $rootScope.current_lecture = lecture;
