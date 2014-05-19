903bb1f5adb549bd68ddabaeef6267162fdd0762
Argo Roots <argo@roots.ee>
2014-05-19 15:40:12 +0300

    Lecture view

--- javascripts/script.js
+++ javascripts/script.js
@@ -8,27 +8,57 @@ function toLecture(data) {
     try        { var date = data.properties.kuupaev.values[0].value; }
     catch(err) { var date = ''; }
 
+    try        { var description = data.properties.kirjeldus.values[0].value; }
+    catch(err) { var description = ''; }
+
     try        { var title = data.properties.nimi.values[0].value; }
     catch(err) { var title = ''; }
 
-    try        { var authors = data.properties.esitaja.values; }
-    catch(err) { var authors = []; }
+    try        { var author = data.properties.esitaja.values; }
+    catch(err) { var author = []; }
+
+    try        { var editor = data.properties.toimetaja.values; }
+    catch(err) { var editor = []; }
+
+    try        { var music = data.properties.muusika.values; }
+    catch(err) { var music = []; }
+
+    try        { var sound = data.properties.heli.values; }
+    catch(err) { var sound = []; }
+
+    try        { var other = data.properties.teised.values; }
+    catch(err) { var other = []; }
 
     try        { var link = data.properties.link.values[0].value; }
     catch(err) { var link = ''; }
 
-    var item_authors = [];
-    for(a in authors) {
-        item_authors.push(authors[a].value);
-    }
+    var item_author = [];
+    for(a in author) { item_author.push(author[a].value); }
+
+    var item_editor = [];
+    for(e in editor) { item_editor.push(editor[e].value); }
+
+    var item_music = [];
+    for(m in music) { item_music.push(music[m].value); }
+
+    var item_sound = [];
+    for(s in sound) { item_sound.push(sound[s].value); }
+
+    var item_other = [];
+    for(o in other) { item_other.push(other[o].value); }
 
     return {
-        id      : data.id,
-        date    : date,
-        year    : date.substring(0, 4),
-        title   : title,
-        authors : item_authors,
-        link    : link,
+        id          : data.id,
+        description : description,
+        date        : date,
+        year        : date.substring(0, 4),
+        title       : title,
+        author      : item_author.sort(),
+        editor      : item_editor.sort(),
+        music       : item_music.sort(),
+        sound       : item_sound.sort(),
+        other       : item_other.sort(),
+        link        : link,
     };
 
 }
@@ -99,9 +129,9 @@ angular.module('ylikoolApp', ['ngRoute'])
                             $rootScope.years = $rootScope.years.reverse();
                         }
 
-                        for(a in lecture.authors) {
-                            if($rootScope.authors.indexOf(lecture.authors[a]) == -1) {
-                                $rootScope.authors.push(lecture.authors[a]);
+                        for(a in lecture.author) {
+                            if($rootScope.authors.indexOf(lecture.author[a]) == -1) {
+                                $rootScope.authors.push(lecture.author[a]);
                                 $rootScope.authors = $rootScope.authors.sort();
                             }
                         }
